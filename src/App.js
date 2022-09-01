cba4240a2cafc5b29439bd94625096e2f5af690c
yasmimc <yasmim.rezende@quintoandar.com.br>
2022-09-01 15:29:00 -0300

    add button and show results

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [query, setQuery] = useState(null);
-  function onKeyUp(e){
+  const [result, setResult] = useState([]);
+  function onKeyUp(e) {
     e.preventDefault();
     setQuery(e.target.value);
     console.log(query);
   }
   function send(e) {
-    e.preventDefault()
-    console.log('lucas 123')
-    const promise = axios.get(`https://sonhos-confinados.herokuapp.com/?${query}`)
-    promise.then((resp)=>console.log(resp.data))
+    e.preventDefault();
+    console.log({ query });
+    axios
+      .get(`https://sonhos-confinados.herokuapp.com/?query=${query}`)
+      .then((resp) => {
+        console.log(resp.data);
+        setResult(resp.data);
+      })
+      .catch((error) => console.log(error));
   }
   return (
     <div className="App">
       <header className="App-header">
-      <form onSubmit={send}>
-      <input
-        type="search"
-        onChange={(e) => onKeyUp(e)}
-        value={query}
-        placeholder={"digite sua query"}
-      />
-      <button type="submit">
-        Enviar</button>
-      </form>
+        <form onSubmit={send}>
+          <input type="search" onChange={(e) => onKeyUp(e)} value={query} placeholder={"digite sua query"} />
+          <button type="submit">Enviar</button>
+        </form>
+        {result === "Query inválida"
+          ? result
+          : result.map((r, i) => (
+              <div>
+                <h3>Resultado Nº {i + 1}</h3>
+                <ul>
+                  {Object.keys(r).map((item) => (
+                    <h6>
+                      {item} : {r[item] ? r[item] : "Sem resposta"}
+                    </h6>
+                  ))}
+                </ul>
+              </div>
+            ))}
       </header>
     </div>
   );
